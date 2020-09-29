0c0526817ced311a26c83a90d62577dd528227ee
Schenikova Snezhana <snezha_00_97@mail.ru>
2020-09-29 10:55:12 +0300

    Update parser_PDF.py

--- parsers/parser_PDF.py
+++ parsers/parser_PDF.py
@@ -3,7 +3,11 @@ import re
 
 
 def pdf_parser(pdf_name):
-    parsedPDF = parser.from_file(pdf_name)
+    try:
+        parsedPDF = parser.from_file(pdf_name)
+    except FileNotFoundError:
+        return 'no such file'
+    
     current_pdf = parsedPDF["content"]
     filtered_pdf = re.sub(r'\\n', '', current_pdf)
     return filtered_pdf
